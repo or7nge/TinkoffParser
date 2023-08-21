eb591f2609e76c99623864408636ac1eaa3db409
msNPS <adgrom@mail.ru>
2023-08-21 22:07:40 +0300

    Changed contest ID

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from bs4 import BeautifulSoup
 from tabulate import tabulate
 from math import sqrt
 import webbrowser
 import requests
 
 LIMIT = 30
+MY_NAME = "summer-2023-7187"
 
-table = requests.get("https://algocode.ru/standings_data/ap_2022").json()
+table = requests.get("https://algocode.ru/standings_data/summer2023_Ap").json()
 
 for user in table["users"]:
-    if user["name"] == "Громыко Андрей":
+    if user["name"] == MY_NAME:
         my_id = str(user["id"])
         print(my_id)
         break
 
 
 class Problem:
-    def __init__(self, contest, title, price):
+    def __init__(self, contest, title):
         if len(contest) <= 30:
             self.contest = contest
         else:
@@ -25,7 +25,6 @@ class Problem:
             self.title = title
         else:
             self.title = title[:40] + "."
-        self.price = price
         self.solved = 0
 
 
@@ -35,33 +34,34 @@ total_solved = 0
 for contest in table["contests"]:
     i_solved = 0
     for problem in contest["problems"]:
+        if "A'" not in problem["short"]:
+            continue
         problems.append(
             Problem(
                 contest["title"],
-                problem["short"] + "." + problem["long"],
-                1 / len(contest["problems"]),
+                problem["short"] + "." + problem["long"]
             )
         )
-        if contest["users"][my_id][problem["index"]]["verdict"] == "OK":
-            total_solved += 1
-            i_solved += 1
-            continue
+        try:
+            if contest["users"][my_id][problem["index"]]["verdict"] == "OK":
+                total_solved += 1
+                i_solved += 1
+                continue
+        except:
+            pass
         for user in contest["users"].values():
             if user[problem["index"]]["verdict"] == "OK":
                 problems[-1].solved += 1
-    mark += i_solved / len(contest["problems"])
-mark = 10 * sqrt(mark / len(table["contests"])) * 0.55 * 0.7
 
-problems.sort(key=lambda problem: -problem.price * problem.solved)
+problems.sort(key=lambda problem: -problem.solved)
 table = []
 for i in problems[:LIMIT]:
-    table.append([i.contest, i.title, i.solved, round(i.price, 2)])
+    table.append([i.contest, i.title, i.solved])
 
 infile = tabulate(
     table, headers=["Контест", "Задача", "Решило", "Цена"], tablefmt="github"
 )
 infile += f"\n\nSOLVED: {total_solved}/{len(problems)}  -  {round(total_solved / len(problems) * 100)}% "
-infile += f"\nMARK: {round(mark, 2)}"
 with open("result.txt", "w", encoding="utf-8") as f:
     f.write(infile)
 
